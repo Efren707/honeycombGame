13b3b2fd1f6230a9c6b999a75d0eb0f5ed93116f
efren <efrenbahen@gmail.com>
2021-10-07 22:58:19 -0500

    testing canvas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
+
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    function canvasDraw(){
+        ctx.fillStyle = '#006A35';
+        ctx.fillRect(10, 10, canvas.height, canvas.width);
+    }
+
+    canvasDraw();
+
 });
